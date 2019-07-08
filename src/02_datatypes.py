9df9032bb1bb2b42e31434536486ea316536d47b
“Josefia <josefia.magagnoli@gmail.com>
2019-07-08 16:15:59 -0400

    Datatypes

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -15,7 +15,20 @@ y = "7"
 
 # YOUR CODE HERE
 
+x = 6
+y = 6
+
+sum = (x) + (y)
+
+print('The sum of {0} and {1} is {2}'.format(x, y, sum))
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+
+x = "5"
+y = "7"
+
+sum = (x) + (y)
+
+print('The sum of strings {0} and {1} is {2}'.format(x, y, sum))
